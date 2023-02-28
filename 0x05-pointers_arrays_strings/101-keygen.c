7d2ee16ce972cf1b9b879a43983152dd35a2fcaa
Techron <86694164+Techron254@users.noreply.github.com>
2023-02-28 04:29:57 +0300

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include <string.h>
-
 /**
- * keygen - generate code to crackme
- *
- * Return: generated code
+ * main - generates keygen.
+ * Return: 0 Always.
  */
-
-int main()
+int main(void)
 {
-	printf("Tada! Congrats");
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
 	return (0);
 }
